42146fae35859b85bbea2667b25d81165207bf6a
RidhoAji921 <ridhoaji921@gmail.com>
2025-08-01 18:25:47 +0700

    list produk

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('products.list');
+        $products = Product::all();
+        return view('products.list', compact('products'));
     }
 
     /**
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -68,7 +68,7 @@
         </div>
     </div>
     
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    <button type="submit" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
     </form>
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
