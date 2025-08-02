ca0f2469a950c893e887751597b6cffa917a3512
RidhoAji921 <ridhoaji921@gmail.com>
2025-08-02 13:54:05 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,22 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::get('/signup', [AuthController::class, 'showRegister'])->name('signup');
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/signup', [AuthController::class, 'showRegister'])->name('signup');
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/products/list', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{product}/manage', [ProductController::class, 'show'])->name('products.show');
-Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-
-Route::post('/products/{product}/transactions', [TransactionController::class, 'store'])->name('transactions.store');
-Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/products/list', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}/manage', [ProductController::class, 'show'])->name('products.show');
+    Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    
+    Route::post('/products/{product}/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+});
